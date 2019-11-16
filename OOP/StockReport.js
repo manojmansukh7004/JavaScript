617aac28bc2e1d2caa2bfb50a20f2d4c321154e2
manojmansukh <manojmansukh7@gmail.com>
2019-11-16 19:34:32 +0530

    json

--- OOP/StockReport.js
+++ OOP/StockReport.js
@@ -1,7 +1,5 @@
 var util=require('./utility/utility');
 var rl=require('readline-sync')
-var value=require('./utility/StockDetails')
-var stock=new value.Stock();
 class Stock{
     constructor(){
         var content=util.readFile('./jsonFile/stock.json');
@@ -13,21 +11,33 @@ class Stock{
 class StockReport extends Stock{
     StockJson(){
         //print Stock Name
-        stock.displayStockName(this.data);
+        this.displayStockName();
         console.log("select the stock number:");
         var ch=util.inputIntegerRead();
         
         //Display stock Details
-        stock.displayStock(ch-1);
+        this.displayStock(ch-1);
         
-        //purches requried shares & calculate the amount. 
+        //purches requried shares & calculate the total stock price 
         var shares = rl.question("Enter no of shares you want to purchase or buy:");
         if(shares>this.data.Stock[ch].StockValue) throw 'enter valid number'
-
-        var totalPrice=stock.claculateStockPrice(ch,shares);
+        var totalPrice=this.claculateStockPrice(ch,shares);
         console.log("Total Stock Price:"+totalPrice);   
     }
-
+    displayStockName(){
+        for(var i=0;i<this.data.Stock.length;i++){
+            console.log((i+1)+"."+this.data.Stock[i].StockName);
+        }
+    }
+    displayStock(ch){
+        console.log("Stock Name :"+this.data.Stock[ch].StockName
+        +"\nNumber Of Shares:"+this.data.Stock[ch].StockValue+"\nStock Price:"
+        +this.data.Stock[ch].StockPrice);
+    }
+    claculateStockPrice(ch,shares){
+        var value=this.data.Stock[ch].StockPrice*shares;
+        return value
+    }
 }
 var stock=new StockReport();
 stock.StockJson();
